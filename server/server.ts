23e2242aba9ab5ff9bd1a3703646c1aed8b71e41
mwangea <mwangeamusa@gmail.com>
2023-10-05 19:46:29 +0300

    fixed some bugs

--- server/server.ts
+++ server/server.ts
@@ -8,7 +8,7 @@ require("dotenv").config();
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_SECRET_KEY,
+    ap_secret: process.env.CLOUD_SECRET_KEY,
 });
 
 //create server 
